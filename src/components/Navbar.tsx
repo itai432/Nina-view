b6e19a2ab13477bee3df7f792732423a380cd31d
itayz1e <itayz1e@gmail.com>
2024-02-22 19:48:39 +0200

    add style folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import  "../style/Navbar.scss";
 
 const Navbar: React.FC = () => {
-  // Define your styles here
-  const styles = {
-    nav: {
-      backgroundColor: "#333",
-      color: "#fff",
-      padding: "0.5rem",
-    },
-    ul: {
-      listStyle: "none",
-      display: "flex",
-      justifyContent: "center",
-      padding: 0,
-    },
-    li: {
-      margin: "0 1rem",
-    },
-    link: {
-      color: "#fff",
-      textDecoration: "none",
-      transition: "color 0.3s ease-in-out",
-    },
-    linkHover: {
-      // This style is for demonstration and won't be directly applied
-      color: "#61dafb",
-    },
-  };
 
   return (
-    <nav style={styles.nav}>
-      <ul style={styles.ul}>
-        <li style={styles.li}>
-          <Link to="/" style={styles.link}>
+    <nav className="nav">
+      <ul className="ul">
+        <li className="li">
+          <Link to="/" className="link">
             Home
           </Link>
         </li>
-        <li style={styles.li}>
-          <Link to="/About-Us" style={styles.link}>
+        <li className="li">
+          <Link to="/About-Us" className="link">
             About Us
           </Link>
         </li>
